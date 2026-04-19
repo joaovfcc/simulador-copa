5c3cd82ac23dc2d23794cd8ac6c62b6ad450fd68
João Farias <vitorfchaves1@gmail.com>
2026-04-19 16:06:43 -0300

    chore: initial project setup and base configuration

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,14 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { gitUserInterceptor } from './core/http/git-user.interceptor';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([gitUserInterceptor]))
   ]
 };
